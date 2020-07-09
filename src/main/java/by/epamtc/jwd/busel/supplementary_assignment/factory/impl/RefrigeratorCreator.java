950ba3c7cdc92c2e9e47f2c45c33612a9eabe241
Siarhei Busiel <sju.busel@gmail.com>
2020-07-10 01:30:37 +0300

    Implement interface Creator in class RefrigeratorCreator

--- src/main/java/by/epamtc/jwd/busel/supplementary_assignment/factory/impl/RefrigeratorCreator.java
+++ src/main/java/by/epamtc/jwd/busel/supplementary_assignment/factory/impl/RefrigeratorCreator.java
@@ -2,13 +2,21 @@ package by.epamtc.jwd.busel.supplementary_assignment.factory.impl;
 
 import by.epamtc.jwd.busel.supplementary_assignment.factory.ApplianceCreator;
 import by.epamtc.jwd.busel.supplementary_assignment.model.Appliance;
+import by.epamtc.jwd.busel.supplementary_assignment.model.impl.Refrigerator;
 
 import java.util.List;
 
 public class RefrigeratorCreator extends ApplianceCreator {
     @Override
     public Appliance createAppliance(List<String> parameterValues) {
-
-        return null;
+        int i = 0;
+        double powerConsumption = Double.parseDouble(parameterValues.get(i++));
+        double weight = Double.parseDouble(parameterValues.get(i++));
+        double freezerCapacity = Double.parseDouble(parameterValues.get(i++));
+        double overallCapacity = Double.parseDouble(parameterValues.get(i++));
+        double height = Double.parseDouble(parameterValues.get(i++));
+        double width = Double.parseDouble(parameterValues.get(i));
+        return new Refrigerator(Appliance.Type.REFRIGERATOR, powerConsumption,
+                weight, freezerCapacity, overallCapacity, height, width);
     }
 }
