d86186f198fc8206d24a171781713c5f0f2efa33
Siarhei Busiel <sju.busel@gmail.com>
2020-07-10 01:38:42 +0300

    Implement interface Creator in class TabletPcCreator

--- src/main/java/by/epamtc/jwd/busel/supplementary_assignment/factory/impl/TabletPcCreator.java
+++ src/main/java/by/epamtc/jwd/busel/supplementary_assignment/factory/impl/TabletPcCreator.java
@@ -2,13 +2,20 @@ package by.epamtc.jwd.busel.supplementary_assignment.factory.impl;
 
 import by.epamtc.jwd.busel.supplementary_assignment.factory.ApplianceCreator;
 import by.epamtc.jwd.busel.supplementary_assignment.model.Appliance;
+import by.epamtc.jwd.busel.supplementary_assignment.model.impl.TabletPc;
 
 import java.util.List;
 
 public class TabletPcCreator extends ApplianceCreator {
     @Override
     public Appliance createAppliance(List<String> parameterValues) {
-
-        return null;
+        int i = 0;
+        double batteryCapacity = Double.parseDouble(parameterValues.get(i++));
+        double displayInches = Double.parseDouble(parameterValues.get(i++));
+        double memoryRom = Double.parseDouble(parameterValues.get(i++));
+        double flashMemoryCapacity = Double.parseDouble(parameterValues.get(i++));
+        String color = parameterValues.get(i);
+        return new TabletPc(Appliance.Type.TABLET_PC, batteryCapacity,
+                displayInches, memoryRom, flashMemoryCapacity, color);
     }
 }
