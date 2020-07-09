d900e23a175a632da6148bd97953906cf4923405
Siarhei Busiel <sju.busel@gmail.com>
2020-07-10 01:42:47 +0300

    Implement interface Creator in class SpeakersCreator

--- src/main/java/by/epamtc/jwd/busel/supplementary_assignment/factory/impl/SpeakersCreator.java
+++ src/main/java/by/epamtc/jwd/busel/supplementary_assignment/factory/impl/SpeakersCreator.java
@@ -2,13 +2,19 @@ package by.epamtc.jwd.busel.supplementary_assignment.factory.impl;
 
 import by.epamtc.jwd.busel.supplementary_assignment.factory.ApplianceCreator;
 import by.epamtc.jwd.busel.supplementary_assignment.model.Appliance;
+import by.epamtc.jwd.busel.supplementary_assignment.model.impl.Speakers;
 
 import java.util.List;
 
 public class SpeakersCreator extends ApplianceCreator {
     @Override
     public Appliance createAppliance(List<String> parameterValues) {
-
-        return null;
+        int i = 0;
+        double powerConsumption = Double.parseDouble(parameterValues.get(i++));
+        int numberOfSpeakers = Integer.parseInt(parameterValues.get(i++));
+        String frequentRange = parameterValues.get(i++);
+        double cordLength = Double.parseDouble(parameterValues.get(i));
+        return new Speakers(Appliance.Type.SPEAKERS, powerConsumption,
+                numberOfSpeakers, frequentRange, cordLength);
     }
 }
