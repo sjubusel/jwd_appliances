588875fed9e142c424b6edeadf2e320a50aaa7b6
Siarhei Busiel <sju.busel@gmail.com>
2020-07-09 23:25:36 +0300

    Replace VacuumCleaner.java to impl-subpackage in model-package

--- src/main/java/by/epamtc/jwd/busel/supplementary_assignment/service/ApplianceFactory.java
+++ src/main/java/by/epamtc/jwd/busel/supplementary_assignment/service/ApplianceFactory.java
@@ -4,7 +4,7 @@ import by.epamtc.jwd.busel.supplementary_assignment.model.Appliance;
 import by.epamtc.jwd.busel.supplementary_assignment.model.impl.Refrigerator;
 import by.epamtc.jwd.busel.supplementary_assignment.model.Speakers;
 import by.epamtc.jwd.busel.supplementary_assignment.model.TabletPc;
-import by.epamtc.jwd.busel.supplementary_assignment.model.VacuumCleaner;
+import by.epamtc.jwd.busel.supplementary_assignment.model.impl.VacuumCleaner;
 import by.epamtc.jwd.busel.supplementary_assignment.model.impl.Laptop;
 import by.epamtc.jwd.busel.supplementary_assignment.model.impl.Oven;
 
