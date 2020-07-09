d89d8b2472e266f8fd11c19b8a026c33802b9bf9
Siarhei Busiel <sju.busel@gmail.com>
2020-07-10 01:26:50 +0300

    Implement interface Creator in class LaptopCreator

--- src/main/java/by/epamtc/jwd/busel/supplementary_assignment/factory/impl/LaptopCreator.java
+++ src/main/java/by/epamtc/jwd/busel/supplementary_assignment/factory/impl/LaptopCreator.java
@@ -2,13 +2,21 @@ package by.epamtc.jwd.busel.supplementary_assignment.factory.impl;
 
 import by.epamtc.jwd.busel.supplementary_assignment.factory.ApplianceCreator;
 import by.epamtc.jwd.busel.supplementary_assignment.model.Appliance;
+import by.epamtc.jwd.busel.supplementary_assignment.model.impl.Laptop;
 
 import java.util.List;
 
 public class LaptopCreator extends ApplianceCreator {
     @Override
     public Appliance createAppliance(List<String> parameterValues) {
-
-        return null;
+        int i = 0;
+        double batteryCapacity = Double.parseDouble(parameterValues.get(i++));
+        String operatingSystem = parameterValues.get(i++);
+        double memoryRom = Double.parseDouble(parameterValues.get(i++));
+        double systemMemory = Double.parseDouble(parameterValues.get(i++));
+        double cpu = Double.parseDouble(parameterValues.get(i++));
+        double displayInches = Double.parseDouble(parameterValues.get(i));
+        return new Laptop(Appliance.Type.LAPTOP, batteryCapacity,
+                operatingSystem, memoryRom, systemMemory, cpu, displayInches);
     }
 }
