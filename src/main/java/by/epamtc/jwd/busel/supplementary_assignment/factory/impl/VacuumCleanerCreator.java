f0e8ff3d6bcb7d62152da17bcdb3480118f59e7c
Siarhei Busiel <sju.busel@gmail.com>
2020-07-10 01:34:51 +0300

    Implement interface Creator in class VacuumCleanerCreator

--- src/main/java/by/epamtc/jwd/busel/supplementary_assignment/factory/impl/VacuumCleanerCreator.java
+++ src/main/java/by/epamtc/jwd/busel/supplementary_assignment/factory/impl/VacuumCleanerCreator.java
@@ -2,13 +2,21 @@ package by.epamtc.jwd.busel.supplementary_assignment.factory.impl;
 
 import by.epamtc.jwd.busel.supplementary_assignment.factory.ApplianceCreator;
 import by.epamtc.jwd.busel.supplementary_assignment.model.Appliance;
+import by.epamtc.jwd.busel.supplementary_assignment.model.impl.VacuumCleaner;
 
 import java.util.List;
 
 public class VacuumCleanerCreator extends ApplianceCreator {
     @Override
     public Appliance createAppliance(List<String> parameterValues) {
-
-        return null;
+        int i = 0;
+        double powerConsumption = Double.parseDouble(parameterValues.get(i++));
+        String filterType = parameterValues.get(i++);
+        String bagType = parameterValues.get(i++);
+        String wandType = parameterValues.get(i++);
+        double motorSpeedRegulation = Double.parseDouble(parameterValues.get(i++));
+        double cleaningWidth = Double.parseDouble(parameterValues.get(i));
+        return new VacuumCleaner(Appliance.Type.VACUUM_CLEANER, powerConsumption,
+                filterType, bagType, wandType, motorSpeedRegulation, cleaningWidth);
     }
 }
